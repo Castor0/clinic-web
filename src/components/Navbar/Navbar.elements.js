886746cf19c853679a8756800589167c0f80d13f
Castor <gregsmarv@gmail.com>
2023-03-27 17:58:00 +0000

    Nav bar (completed with Functionality)

--- src/components/Navbar/Navbar.elements.js
+++ src/components/Navbar/Navbar.elements.js
@@ -6,7 +6,7 @@ import { Container } from '../../globalStyles';
 
 
 export const Nav = styled.nav`
-background: #180014;
+background: #0a0109;
 height: 80px;
 display: flex;
 justify-content: center;
@@ -56,3 +56,87 @@ display: none;
     cursor: pointer;   
 }
 `;
+
+export const NavMenu =styled.ul`
+display: flex;
+align-items: center;
+list-style: none;
+text-align: center;
+
+
+@media screen and (max-width: 960px) {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 90vh;
+    position: absolute;
+    top: 80px;
+left: ${({click}) => (click ? 0 : '-100%')};
+opacity: 1;
+transition: all 0.5s ease;
+background: #101522;
+}
+`;
+
+export const NavItem =styled.li`
+height: 80px;
+border-bottom: 2px solid transparent;
+
+&:hover {
+    border-bottom: 2px solid #4b59f7;
+}
+
+
+@media screen and (max-width: 960px) {
+    width: 100%;
+
+    &:hover {
+        border: none;
+    }
+}
+`
+
+export const NavLinks = styled(Link)`
+color: white;
+display: flex;
+align-items: center;
+text-decoration: none;
+padding: 0.5rem 1rem;
+height: 100%;
+
+@media screen and (max-width: 960px) {
+    text-align: center;
+    padding: 2rem;
+    width: 100%;
+    display: table;
+
+
+    &:hover{
+        color: #4b59f7;
+        transition: all 0.3s ease;
+    }
+}
+`
+export const NavItemBtn = styled.li`
+@media screen and (max-width: 960px) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 120px;
+}
+`
+
+export const NavBtnLink = styled(Link)`
+display: flex;
+justify-content: center;
+align-items: center;
+text-decoration: none;
+padding: 8px 16px;
+height: 100%;
+width: 100%;
+border: none;
+outline: none;
+`
+
+
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,39 @@
-import React, {useState} from 'react'
-import { Nav, NavBarContainer, NavLogo, NavIcon,MobileIcon} from './Navbar.elements'
-import { FaTimes, FaBars } from 'react-icons/fa'
-import { IconContext } from 'react-icons/lib'
+import React, {useState} from 'react';
+import { FaTimes, FaBars } from 'react-icons/fa';
+import { IconContext } from 'react-icons/lib';
+import { Button } from '../../globalStyles'
+import { 
+  Nav, 
+  NavBarContainer, 
+  NavLogo, 
+  NavIcon, 
+  MobileIcon, 
+  NavMenu, 
+  NavItem, 
+  NavLinks,
+  NavBtnLink, 
+  NavItemBtn,
+} from './Navbar.elements'
+
 
 const Navbar = () => {
 
  const [click, setClick] = useState(false)
+ const [button, setButton] = useState(true)
+
+
 
  const handleClick = () => setClick(!click);
 
+ const showButton = () => {
+  if (window.innerWidth <= 960) {
+    setButton(false);
+  } else {
+    setButton(true);
+  }
+ }
+
+ window.addEventListener('resize', showButton);
   
   return (
     <>
@@ -24,6 +49,28 @@ const Navbar = () => {
                 >
                   { click ? <FaTimes /> : <FaBars />}
                 </MobileIcon>
+                <NavMenu onClick={handleClick} click={click}> 
+                  <NavItem>
+                    <NavLinks to='/'>Home</NavLinks>
+                  </NavItem>
+                  <NavItem>
+                    <NavLinks to='/about'>About Us</NavLinks>
+                  </NavItem>
+                  <NavItem>
+                    <NavLinks to='/services'>Services</NavLinks>
+                  </NavItem>
+                  <NavItemBtn>
+                    {button ? (
+                      <NavBtnLink to='/book-appointment'>
+                        <Button primary> Book Now</Button>
+                      </NavBtnLink>
+                    ): (
+                      <NavBtnLink to ='/book-appointment'>
+                        <Button fontBig primary> Book Appointment </Button>
+                      </NavBtnLink>
+                    )}
+                  </NavItemBtn>
+                </NavMenu>
             </NavBarContainer>
           </Nav>
         </IconContext.Provider>
