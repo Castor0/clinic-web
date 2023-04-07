13018223b4c8d2a461a4fc99a1799a7d466862d9
Castor <gregsmarv@gmail.com>
2023-04-07 14:58:05 +0000

    pricing component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,105 +1,116 @@
 import React from 'react';
-import {
-  FaFacebook, FaInstagram, FaLinkedin, FaTwitter} from 'react-icons/fa';
 import { Button } from '../../globalStyles';
-import { 
+import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaTwitter,
+  FaLinkedin
+} from 'react-icons/fa';
+import {
   FooterContainer,
   FooterSubscription,
-  FooterSubHeading,
   FooterSubText,
+  FooterSubHeading,
   Form,
   FormInput,
   FooterLinksContainer,
   FooterLinksWrapper,
-  FooterLinksItems,
-  FooterLink,
+  FooterLinkItems,
   FooterLinkTitle,
+  FooterLink,
   SocialMedia,
+  SocialMediaWrap,
   SocialLogo,
   SocialIcon,
   WebsiteRights,
   SocialIcons,
-  SocialIconLink,
+  SocialIconLink
 } from './Footer.elements';
 
-const Footer = () => {
+function Footer() {
   return (
-   <FooterContainer>
+    <FooterContainer>
       <FooterSubscription>
-          <FooterSubHeading>
-          We don’t want to just give you and your loved ones beautiful & healthy relaxed when you come to see us comes down to the three
-          </FooterSubHeading>
-          <FooterSubText>
-              You can unsubscribe at any time.
-          </FooterSubText>
-          <Form>
-            <FormInput name="email" type="email" placeholder="Your Email" />
-              <Button fontBig>
-                Subscribe
-              </Button>
-          </Form>
+        <FooterSubHeading>
+          Join our exclusive membership to receive the latest news and trends
+        </FooterSubHeading>
+        <FooterSubText>You can unsubscribe at any time.</FooterSubText>
+        <Form>
+          <FormInput name='email' type='email' placeholder='Your Email' />
+          <Button fontBig>Subscribe</Button>
+        </Form>
       </FooterSubscription>
       <FooterLinksContainer>
         <FooterLinksWrapper>
-          <FooterLinksItems>
+          <FooterLinkItems>
             <FooterLinkTitle>About Us</FooterLinkTitle>
             <FooterLink to='/sign-up'>How it works</FooterLink>
             <FooterLink to='/'>Testimonials</FooterLink>
             <FooterLink to='/'>Careers</FooterLink>
-            <FooterLink to='/'>How it works</FooterLink>
+            <FooterLink to='/'>Investors</FooterLink>
             <FooterLink to='/'>Terms of Service</FooterLink>
-          </FooterLinksItems>
-          <FooterLinksItems>
+          </FooterLinkItems>
+          <FooterLinkItems>
             <FooterLinkTitle>Contact Us</FooterLinkTitle>
-            <FooterLink to='/sign-up'>How it works</FooterLink>
-            <FooterLink to='/'>Testimonials</FooterLink>
-            <FooterLink to='/'>Careers</FooterLink>
-            <FooterLink to='/'>How it works</FooterLink>
-            <FooterLink to='/'>Terms of Service</FooterLink>
-          </FooterLinksItems>
-          </FooterLinksWrapper>
-          <FooterLinksWrapper>
-          <FooterLinksItems>
-            <FooterLinkTitle>Videos</FooterLinkTitle>
-            <FooterLink to='/sign-up'>How it works</FooterLink>
-            <FooterLink to='/'>Testimonials</FooterLink>
-            <FooterLink to='/'>Careers</FooterLink>
-            <FooterLink to='/'>How it works</FooterLink>
-            <FooterLink to='/'>Terms of Service</FooterLink>
-          </FooterLinksItems>
-          <FooterLinksItems>
-            <FooterLinkTitle>Social Media</FooterLinkTitle>
-            <FooterLink to='/sign-up'>How it works</FooterLink>
-            <FooterLink to='/'>Testimonials</FooterLink>
-            <FooterLink to='/'>Careers</FooterLink>
-            <FooterLink to='/'>How it works</FooterLink>
-            <FooterLink to='/'>Terms of Service</FooterLink>
-          </FooterLinksItems>
+            <FooterLink to='/'>Contact</FooterLink>
+            <FooterLink to='/'>Support</FooterLink>
+            <FooterLink to='/'>Destinations</FooterLink>
+            <FooterLink to='/'>Sponsorships</FooterLink>
+          </FooterLinkItems>
         </FooterLinksWrapper>
-        <SocialMedia>
+        <FooterLinksWrapper>
+          <FooterLinkItems>
+            <FooterLinkTitle>Videos</FooterLinkTitle>
+            <FooterLink to='/'>Submit Video</FooterLink>
+            <FooterLink to='/'>Ambassadors</FooterLink>
+            <FooterLink to='/'>Agency</FooterLink>
+            <FooterLink to='/'>Influencer</FooterLink>
+          </FooterLinkItems>
+          <FooterLinkItems>
+            <FooterLinkTitle>Social Media</FooterLinkTitle>
+            <FooterLink to='/'>Instagram</FooterLink>
+            <FooterLink to='/'>Facebook</FooterLink>
+            <FooterLink to='/'>Youtube</FooterLink>
+            <FooterLink to='/'>Twitter</FooterLink>
+          </FooterLinkItems>
+        </FooterLinksWrapper>
+      </FooterLinksContainer>
+      <SocialMedia>
+        <SocialMediaWrap>
           <SocialLogo to='/'>
             <SocialIcon />
               KIM CLEMENS DENTAL CLINIC
           </SocialLogo>
-          <WebsiteRights>KIM CLEMENS DENTAL CLINIC @ 2023</WebsiteRights>
+          <WebsiteRights>K.C.D.C © 2020</WebsiteRights>
           <SocialIcons>
-            <SocialIconLink href='/' target='_blank' aria-label="Facebook"> 
+            <SocialIconLink href='/' target='_blank' aria-label='Facebook'>
               <FaFacebook />
             </SocialIconLink>
-            <SocialIconLink href='/' target='_blank' aria-label="Twitter"> 
-              <FaTwitter />
-            </SocialIconLink>
-            <SocialIconLink href='/' target='_blank' aria-label="Instagram"> 
+            <SocialIconLink href='/' target='_blank' aria-label='Instagram'>
               <FaInstagram />
             </SocialIconLink>
-            <SocialIconLink href='/' target='_blank' aria-label="Linkedin"> 
+            <SocialIconLink
+              href={
+                '//www.youtube.com/channel/UCsKsymTY_4BYR-wytLjex7A?view_as=subscriber'
+              }
+              rel='noopener noreferrer'
+              target='_blank'
+              aria-label='Youtube'
+            >
+              <FaYoutube />
+            </SocialIconLink>
+            <SocialIconLink href='/' target='_blank' aria-label='Twitter'>
+              <FaTwitter />
+            </SocialIconLink>
+            <SocialIconLink href='/' target='_blank' aria-label='LinkedIn'>
               <FaLinkedin />
             </SocialIconLink>
           </SocialIcons>
-        </SocialMedia>
-      </FooterLinksContainer>
-   </FooterContainer>
-  )
+        </SocialMediaWrap>
+      </SocialMedia>
+    </FooterContainer>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { homeObjOne, homeObjTwo, homeObjThree,homeObjFour } from './Data'
-import {InfoSection} from '../../components'
+import {InfoSection, Pricing} from '../../components'
 const Home = () => {
   return (
    <>
     <InfoSection  {...homeObjOne}/>
     <InfoSection  {...homeObjTwo}/>
     <InfoSection  {...homeObjThree}/>
-    <InfoSection  {...homeObjFour}/>
+    <Pricing />
+    
 
    </>
   )
